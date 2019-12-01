c27b86727b946c321064190994644a33524d020c
Williams Estrada <w.estrada.r@outlook.com>
2019-11-30 23:29:40 -0600

    Add 'underpopulation' rule
    This create the method 'underpopulation' in the class Game, this method
    receives a cell and returns it with its property 'next_state' changed
    following the underpopulation rule; "Any live cell with fewer than two
    live neighbours dies, as if by underpopulation"

--- game.py
+++ game.py
@@ -6,6 +6,12 @@ class Game:
     def __init__(self, board = []):
         self.board = board
 
+    def underpopulation(self, cell):
+        if cell.state == 'alive':
+            if cell.live_neighbours < 2:
+                cell.next_state = 'dead'
+        return cell
+
     def print_board(self):
         for row in self.board:
             for cell in row:
--- test_game.py
+++ test_game.py
@@ -2,8 +2,26 @@
 # -*- coding: utf-8 -*-
 
 import unittest
+from game import Game
+from cell import Cell
 
 class TestGame(unittest.TestCase):
+
+    def setUp(self):
+        self.cell = Cell()
+        self.game = Game()
+
+    def test_underpopulation(self):
+        self.cell.state = 'alive'
+        self.cell.live_neighbours = 1
+        self.cell = self.game.underpopulation(self.cell)
+
+        self.assertEqual(self.cell.next_state, 'dead')
+
+        self.cell.live_neighbours = 0
+        self.cell = self.game.underpopulation(self.cell)
+
+        self.assertEqual(self.cell.next_state, 'dead')
 
 if __name__ == '__main__':
     unittest.main()
