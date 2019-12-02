194c23132bb51cb78d3b957875779d6da868cfe1
Williams Estrada <w.estrada.r@outlook.com>
2019-12-02 10:29:42 -0600

    Add method 'set_board'
    This method takes a list and return a square matrix of the size given as
    parameter, the matrix is filled with instances of the Cell class

--- main.py
+++ main.py
@@ -5,13 +5,18 @@ from game import Game
 from cell import Cell
 
 def main():
-    board = [
-            [Cell(), Cell()],
-            [Cell(), Cell()]
-            ]
+    board = set_board([], 10)
     game = Game(board)
     print('Board')
     game.print_board()
+
+def set_board(board, size):
+    for i in range(size):
+        board.append([])
+        for j in range(size):
+            board[i].append(Cell())
+
+    return board;
 
 
 if __name__ == '__main__':
--- test_main.py
+++ test_main.py
@@ -2,6 +2,31 @@
 # -*- coding: utf-8 -*-
 
 import unittest
+import main
+from cell import Cell
 
-class TestMain(unnittest.TestCase):
+class TestMain(unittest.TestCase):
+    
+    def setUp(self):
+        self.board = main.set_board([], 10)
 
+    def test_board_is_a_square_matrix(self):
+        self.assertEqual(len(self.board), 10)
+        self.assertEqual(len(self.board[0]), 10)
+
+        self.board = main.set_board([], 5)
+
+        self.assertEqual(len(self.board), 5)
+        self.assertEqual(len(self.board[0]), 5)
+
+    def test_board_is_a_matrix_of_cells(self):
+        self.assertEqual(self.list_elements_instance_of(self.board, list), True)
+
+        for row in self.board:
+            self.assertEqual(self.list_elements_instance_of(row, Cell), True)
+
+    def list_elements_instance_of(self, list, type):
+        return all(isinstance(x, type) for x in list)
+
+if __name__ == '__main__':
+    unittest.main()
