77116c26391d4bdae8d088d762aca3088611b05f
Williams Estrada <w.estrada.r@outlook.com>
2019-12-03 00:51:52 -0600

    Move board related method to Game class
    This is done since the game logic should be on the Game class

--- game.py
+++ game.py
@@ -1,10 +1,56 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+from cell import Cell
+
 class Game:
 
-    def __init__(self, board = []):
-        self.board = board
+    def __init__(self, board_size):
+        self.board = self.set_board([], board_size)
+
+
+    def set_board(self, board, size):
+        for i in range(size):
+            board.append([])
+            for j in range(size):
+                board[i].append(Cell())
+
+        for i in range(size):
+            for j in range(size):
+                cell = self.set_neighbours(board, [i,j])
+                board[i][j] = cell
+
+        return board;
+
+
+    def set_neighbours(self, board, position):
+        limit_x = len(board) - 1
+        limit_y = len(board[0]) - 1
+
+        cell = board[position[0]][position[1]]
+
+        start_x = self.delimit_number(position[0] - 1, limit_x)
+        start_y = self.delimit_number(position[1] - 1, limit_y)
+
+        end_x = self.delimit_number(position[0] + 1, limit_x)
+        end_y = self.delimit_number(position[1] + 1, limit_y)
+
+        for i in range(start_x, end_x + 1):
+            for j in range(start_y, end_y + 1):
+                neighbour = board[i][j]
+                if not cell == neighbour:
+                    if not cell in neighbour.neighbours:
+                        neighbour.neighbours.append(cell)
+                        cell.neighbours.append(neighbour)
+
+        return cell
+
+
+    def delimit_number(self, number, limit):
+        if number < 0: return 0;
+        if number > limit: return limit;
+        return number
+
 
     def next_step(self):
         for row in self.board:
--- test_game.py
+++ test_game.py
@@ -9,7 +9,8 @@ class TestGame(unittest.TestCase):
 
     def setUp(self):
         self.cell = Cell()
-        self.game = Game()
+        self.game = Game(10)
+
 
     def test_next_step(self):
         c1 = Cell(state = 'alive', live_neighbours = 1)
@@ -28,6 +29,7 @@ class TestGame(unittest.TestCase):
         self.assertEqual(board[1][0].state, 'alive')
         self.assertEqual(board[1][1].state, 'dead')
 
+
     def test_underpopulation(self):
         self.cell.state = 'alive'
         self.cell.live_neighbours = 1
@@ -40,12 +42,14 @@ class TestGame(unittest.TestCase):
 
         self.assertEqual(self.cell.next_state, 'dead')
 
+
     def test_overpopulation(self):
         self.cell.state = 'alive'
         self.cell.live_neighbours = 4
         self.cell = self.game.check_rules(self.cell)
 
         self.assertEqual(self.cell.next_state, 'dead')
+
 
     def test_survive(self):
         self.cell.state = 'alive'
@@ -59,12 +63,109 @@ class TestGame(unittest.TestCase):
 
         self.assertEqual(self.cell.next_state, 'alive')
 
+
     def test_become_alive(self):
         self.cell.state = 'dead'
         self.cell.live_neighbours = 3
         self.cell = self.game.check_rules(self.cell)
 
         self.assertEqual(self.cell.next_state, 'alive')
+
+
+    def test_board_is_a_square_matrix(self):
+        self.assertEqual(len(self.game.board), 10)
+        self.assertEqual(len(self.game.board[0]), 10)
+
+        self.game.board = self.game.set_board([], 5)
+
+        self.assertEqual(len(self.game.board), 5)
+        self.assertEqual(len(self.game.board[0]), 5)
+
+
+    def test_board_is_a_matrix_of_cells(self):
+        self.assertEqual(self.list_elements_instance_of(self.game.board, list), True)
+
+        for row in self.game.board:
+            self.assertEqual(self.list_elements_instance_of(row, Cell), True)
+
+
+    def test_board_sets_neighbours(self):
+        cell = self.game.board[5][5]
+        self.assertEqual(len(cell.neighbours), 8)
+
+
+    def test_neighbour_change_state(self):
+        self.game.board[0][1].state = 'alive'
+        self.assertEqual(self.game.board[0][0].live_neighbours, 1)
+
+
+    def test_set_neighours_return_a_cell(self):
+        cell = self.game.set_neighbours(self.game.board, [0,0])
+
+        self.assertEqual(isinstance(cell, Cell), True)
+
+
+    def test_set_right_amount_of_neighours(self):
+        cell = self.game.set_neighbours(self.game.board, [0,0])
+        self.assertEqual(len(cell.neighbours), 3)
+
+        cell = self.game.set_neighbours(self.game.board, [0,5])
+        self.assertEqual(len(cell.neighbours), 5)
+
+        cell = self.game.set_neighbours(self.game.board, [0,9])
+        self.assertEqual(len(cell.neighbours), 3)
+
+        cell = self.game.set_neighbours(self.game.board, [5,0])
+        self.assertEqual(len(cell.neighbours), 5)
+
+        cell = self.game.set_neighbours(self.game.board, [5,5])
+        self.assertEqual(len(cell.neighbours), 8)
+
+        cell = self.game.set_neighbours(self.game.board, [5,9])
+        self.assertEqual(len(cell.neighbours), 5)
+
+        cell = self.game.set_neighbours(self.game.board, [5,9])
+        self.assertEqual(len(cell.neighbours), 5)
+
+        cell = self.game.set_neighbours(self.game.board, [9,0])
+        self.assertEqual(len(cell.neighbours), 3)
+
+        cell = self.game.set_neighbours(self.game.board, [9,5])
+        self.assertEqual(len(cell.neighbours), 5)
+
+        cell = self.game.set_neighbours(self.game.board, [9,9])
+        self.assertEqual(len(cell.neighbours), 3)
+
+
+    def test_duplicate_set_neighbours(self):
+        cell = self.game.set_neighbours(self.game.board, [0,0])
+        cell = self.game.set_neighbours(self.game.board, [0,0])
+        self.assertEqual(len(cell.neighbours), 3)
+
+        c1 = self.game.set_neighbours(self.game.board, [5,5])
+        c2 = self.game.set_neighbours(self.game.board, [5,6])
+        self.assertEqual(len(c1.neighbours), 8)
+
+    def test_linked_negihbours(self):
+        cell = self.game.set_neighbours(self.game.board, [5,5])
+        neighbour = cell.neighbours[0]
+        self.assertEqual(cell in neighbour.neighbours, True)
+        self.assertEqual(neighbour in cell.neighbours, True)
+
+
+    def test_delimit_number(self):
+        number = self.game.delimit_number(-1, 28)
+        self.assertEqual(number, 0)
+
+        number = self.game.delimit_number(7, 5)
+        self.assertEqual(number, 5)
+
+        number = self.game.delimit_number(3, 5)
+        self.assertEqual(number, 3)
+
+
+    def list_elements_instance_of(self, list, type):
+        return all(isinstance(x, type) for x in list)
 
 if __name__ == '__main__':
     unittest.main()
